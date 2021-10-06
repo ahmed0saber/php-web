33d287812843cb128207f7e4a23a17f56a6a2cb9
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-07 00:45:24 +0200

    Create index.php

---
+++ keep-data-after-submitting/index.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <style>
+            .error{
+                color: #f00;
+            }
+        </style>
+    </head>
+<body>
+<?php
+$name = $email = $nameErr = $emailErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+      } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
+      }
+    
+      if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+      } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $emailErr = "Invalid email format";
+        }
+      }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+Name:<input type="text" name="name" value="<?php echo $name;?>">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail:<input type="text" name="email" value="<?php echo $email;?>">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+<input type="submit">
+</form>
+</body>
+</html>
