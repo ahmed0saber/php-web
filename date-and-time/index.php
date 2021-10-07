eaade578fade651f8bcece3ae29498959280196e
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-07 02:46:35 +0200

    Create index.php

---
+++ date-and-time/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo "The time is " . date("h:i:sa");
+?>
+</body>
+</html>
