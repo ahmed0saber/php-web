eea70d0a4ac7d6ea8d113ebe2722ba9e3fbe0029
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-15 20:05:32 +0200

    Update index.php

--- frequency-of-certain-word-in-text-file/index.php
+++ frequency-of-certain-word-in-text-file/index.php
@@ -4,6 +4,8 @@ $myString = fread($myfile,filesize("file.txt"));
 if (strpos($myString, 'Ahmed') !== false) {
     print_r(array_count_values(str_word_count($myString, 1))['Ahmed']);
     echo ' words found';
+}else{
+    echo 'Not found';
 }
 fclose($myfile);
 ?>
