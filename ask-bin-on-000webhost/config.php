bfbdd705e321181384890d67904f834feccafae0
Ahmed Saber <64714761+ahmed0saber@users.noreply.github.com>
2021-10-26 07:49:42 +0200

    Update config.php

--- ask-bin-on-000webhost/config.php
+++ ask-bin-on-000webhost/config.php
@@ -1,9 +1,9 @@
 <?php 
   //data required for connection of database to log in and sign up
 $server = "localhost";
-$user = "id17820765_root";
-$pass = "]QvV)9{=SVb8RSz*";
-$database = "id17820765_askbin";
+$user = "root";
+$pass = "";
+$database = "askbin";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
